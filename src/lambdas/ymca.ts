c76cd098ecfe53b51e4a078fc82bf08792603720
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-02-04 01:46:36 +0530

    feat: :sparkles: add service function type

--- src/lambdas/ymca.ts
+++ src/lambdas/ymca.ts
@@ -1,6 +1,8 @@
 import { ApiHandler } from "sst/node/api";
 
 export const YMCAHandler = ApiHandler(async () => {
+	// We can extract it via functions that are adaptive to situations
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
---
+++ src/types/api/service-function.ts
@@ -0,0 +1,7 @@
+// TODO: Discus if to keep all the params like req -> query, params and body separate or the same. As of now keeping the same.
+
+import type { APIResponse } from ".";
+
+export type ServiceFunction<D = void, R = Record<string, never>> = (
+	args: R,
+) => Promise<APIResponse<D>>;
